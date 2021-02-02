3ef2d8fb0d02d11cb3b677e5b8e6a9d156c346e0
anace-m <maldonadoanaceciliac@johndeere.com>
2021-02-02 16:30:49 -0600

    Seeded BodyParts table

--- seeders/20210202222430-demo-bodyparts.js
+++ seeders/20210202222430-demo-bodyparts.js
@@ -2,23 +2,23 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  await queryInterface.bulkInsert('BodyParts', [
+    {
+      name: "Bear Legs"
+    },
+    {
+      name: "Bear Body"
+    },
+    {
+      name: "Bear Arms"
+    },
+    {
+      name: "Bear Head"
+    }
+], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('BodyParts', null, {});
   }
 };
