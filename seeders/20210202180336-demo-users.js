dec0bd8673849eaec220b9c1f16d96f9ba16b5a5
anace-m <maldonadoanaceciliac@johndeere.com>
2021-02-02 12:16:11 -0600

    Seeded Users table

--- seeders/20210202180336-demo-users.js
+++ seeders/20210202180336-demo-users.js
@@ -2,23 +2,21 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Users', [
+      {
+        name:'Matt',
+        username: 'guffy',
+        password: '123',
+    },
+    {
+        name:'Ana Cecilia',
+        username: 'anace',
+        password: 'acmc',
+    }
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Users', null, {});
   }
 };
