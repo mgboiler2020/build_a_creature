cb25c468a59d4df924be2a50b2cd71006e93dd85
anace-m <maldonadoanaceciliac@johndeere.com>
2021-02-01 19:26:40 -0600

    Updated server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+//REQUIRES
+const { request } = require('express');
+const express = require("express");
+const app = express();
+const methodOverride = require("method-override");
+const routes = require("./routes");
+
+/*
+//Data
+ const pokemon = require("./models/pokemon.js");
+ const players = require("./models/player.js");
+ const teams = require("./models/team.js");
+ */
+
+//MIDDLEWARE
+//To confirm route
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
+});
+//To convert data submitted into a format the server can read
+app.use(express.urlencoded({ extended: true }));
+//To delete/exit existing data
+app.use(methodOverride("_method"));
+//To serve static file (CSS)
+app.use(express.static(__dirname + "/public"));
+
+/*
+app.use("/pokemon", routes.pokemon);
+app.use("/players", routes.players);
+app.use("/teams", routes.teams);
+*/
+
+//LISTEN
+app.listen(3000, () => {
+    console.log("My build-a-creature app is set up and running");
+});
