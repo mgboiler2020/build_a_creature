56d180633caa027d996b6f524c0cf301ea28f218
MattG2000 <GuffyMatthewO@JohnDeere.com>
2021-02-04 09:22:30 -0500

    Resolved link to Build

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const ctrl = require('../controllers');
 
 router.get('/', ctrl.users.index);
 router.get('/signup', ctrl.users.renderSign);
+router.get('/build', ctrl.users.renderIndex);
 router.post('/signup', ctrl.users.signUp);
 router.get('/profile/:index', ctrl.users.renderProfile);
 router.get('/login', ctrl.users.renderLogin);
